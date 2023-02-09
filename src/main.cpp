2c0b54b2702405c521b1fd8c183f79a8d9702977
Anthony <acgardner95@gmail.com>
2023-02-09 00:18:18 -0800

    Add vector boilerplate

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Vector.h"
+#include <cmath>
+
+
+double sqrt_sum(const Vector& v)
+{
+    double sum = 0;
+    for (int i=0; i!=v.size(); ++i)
+    {
+        sum += std::sqrt(v[i]);
+    }
+    return sum;
+}
